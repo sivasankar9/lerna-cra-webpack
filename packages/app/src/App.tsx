5fdea933ba013d43272c85315c7fbc9cc35434d4
sivasankar <sivasankar.g@perfios.com>
2022-12-13 13:18:01 +0530

    fix

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { Button, Text } from "@todcode/components";
+import { Button, Text } from "@perfios/components";
 
 function App() {
   const [count, setCount] = useState(0);
